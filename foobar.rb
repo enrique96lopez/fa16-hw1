2b1da4a49219116af2103c4cbc0c2f05f9464b97
enrique96lopez <enrique96lopez@gmail.com>
2016-09-20 23:24:34 -0700

    Add files via upload

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,15 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    add=0
+    first =a.map { |string|	(string.to_i) +2}
+	second= first.uniq
+	third = second.select {|i| i <= 10 } 
+	fourth= third.select{|i| i%2==0}
+	fourth.each{|num| add+=num}
+	return add
   end
+
 end
 
 
